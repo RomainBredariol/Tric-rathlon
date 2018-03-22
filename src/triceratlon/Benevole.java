c70357dc9df78549a3344e102d7d72ff06929b11
Breda <romain.bredariol@gmail.com>
2018-03-22 16:27:07 +0100

    ajout getter setter à la classe bénévole

--- src/triceratlon/Benevole.java
+++ src/triceratlon/Benevole.java
@@ -9,17 +9,58 @@ public class Benevole {
 	private String commentaire;
 	
 	public Benevole(String nom, String prénom, String mail, String tel, String commentaire) {
-		this.nom=nom;
-		this.prénom=prénom;
-		this.mail=mail;
-		this.tel=tel;
-		this.commentaire=commentaire;
+		this.setNom(nom);
+		this.setPrénom(prénom);
+		this.setMail(mail);
+		this.setTel(tel);
+		this.setCommentaire(commentaire);
 	}
 	
 	public void affecterAUneTache(Tache tache) {
 		System.out.println("ta mere la pute");
 	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrénom() {
+		return prénom;
+	}
+
+	public void setPrénom(String prénom) {
+		this.prénom = prénom;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getCommentaire() {
+		return commentaire;
+	}
+
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
 	
+	s
 	
 	
 }
