8e4633f1f901a1df16c69492a67643cdb86a707a
Breda <romain.bredariol@gmail.com>
2018-03-24 12:36:39 +0100

    Ajout Classe ecrire, lire, supprimer en base et test: tout est ok

---
+++ src/BDD/TestBDD.java
@@ -0,0 +1,13 @@
+package BDD;
+
+public class TestBDD {
+
+	public static void main(String[] args) {
+		EcrireEnBase ajout = new EcrireEnBase("tata");
+		LireEnBase lecture = new LireEnBase("user");
+		SupprimerTouteLaColonne supp = new SupprimerTouteLaColonne();
+		LireEnBase lec = new LireEnBase("user");
+
+	}
+
+}
