6b9b18d5d9597691cb88bfd1088f917a5fed1315
Breda <romain.bredariol@gmail.com>
2018-03-26 09:34:52 +0200

    Suppression Ecrire et Lire EnBase + modif Test

--- src/BDD/TestBDD.java
+++ src/BDD/TestBDD.java
@@ -3,10 +3,8 @@ package BDD;
 public class TestBDD {
 
 	public static void main(String[] args) {
-		EcrireEnBase ajout = new EcrireEnBase("tata");
-		LireEnBase lecture = new LireEnBase("user");
-		SupprimerTouteLaColonne supp = new SupprimerTouteLaColonne();
-		LireEnBase lec = new LireEnBase("user");
+		SqlRequete rq = new SqlRequete();
+		rq.Connect();
 
 	}
 
