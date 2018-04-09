ad558945be20fb01c5153464e3cb2b14c6c854f5
Breda <romain.bredariol@gmail.com>
2018-04-09 18:27:25 +0200

    Ajout des classes main app etc pour java fx

---
+++ src/Pages/MainApp.java
@@ -0,0 +1,65 @@
+package Pages;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Test JavaFX");
+
+		showPersonOverview();
+		
+	}
+
+
+	public void showPersonOverview() {
+		try {
+			// Load person overview.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("Accueil_1ereConnexion.fxml"));
+			AnchorPane personOverview = (AnchorPane) loader.load();
+			accueilControleur controleur = loader.getController();
+			controleur.setMainApp(this);
+			controleur.setModele();
+			
+			Scene scene = new Scene(personOverview);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+
+			// Set person overview into the center of root layout.
+			//rootLayout.setCenter(personOverview);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Returns the main stage.
+	 * 
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
